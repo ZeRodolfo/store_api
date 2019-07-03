877d9e9ba29e2e3b451a146d14fe05d84228f535
José Rodolfo <j.rodolfopps@gmail.com>
2019-07-03 11:37:56 -0300

    Acesso somente para quem está logado na api

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,14 +2,14 @@
 
 namespace App\Entity;
 
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\OAuthServerBundle\Entity\Client as BaseClient;
 use Doctrine\ORM\Mapping as ORM;
+
 
 /** 
  * @ORM\Entity
- * @ORM\Table(name="fos_user")
  */
-class User extends BaseUser {
+class Client extends BaseClient {
   /** 
    * @ORM\Id
    * @ORM\Column(type="integer")
